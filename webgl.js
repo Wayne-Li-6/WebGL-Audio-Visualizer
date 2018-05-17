67beb025ab6b537551713a525a649bfcb8a94e88
wyl6 <wyl6@cornell.edu>
2018-05-16 22:09:26 -0400

    Implemented bloom filtering for wiremesh mode

--- webgl.js
+++ webgl.js
@@ -63,6 +63,65 @@ function createGlslProgram(gl, vertexShaderId, fragmentShaderId) {
 }
 
 /**
+ * Function that creates and draws a full screen quad and draws the 2D [texture]
+ * on top of said quad. [texture2] is used in the final step of the bloom filter
+ * when combining the blurred image and the original image; [combine] is true.
+ */
+function createAndDrawFullScreenQuad(gl, program, texture, original_texture, combine) {
+    var vertices = [
+        -1.0, -1.0, 0.0,
+        0.0, 0.0,
+        1.0, -1.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0, 0.0,
+        1.0, 1.0,
+        -1.0, 1.0, 0.0,
+        0.0, 1.0
+    ];
+    var indices = [ 0, 1, 2, 0, 2, 3 ];
+
+    var vertexArray = new Float32Array(vertices);
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertexArray, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    var indexArray = new Uint16Array(indices);
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indexArray, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+    gl.useProgram(program);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+    if (combine) {
+        gl.activeTexture(gl.TEXTURE1);
+        gl.bindTexture(gl.TEXTURE_2D, original_texture);
+        gl.uniform1i(gl.getUniformLocation(program, "original_texture"), 1);
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    var vertPosition = gl.getAttribLocation(program, "vert_position");
+    gl.enableVertexAttribArray(vertPosition);
+    gl.vertexAttribPointer(vertPosition, 3, gl.FLOAT, false, 4 * 5, 0);
+    var vertTexCoord = gl.getAttribLocation(program, "vert_texCoord");
+    gl.enableVertexAttribArray(vertTexCoord);
+    gl.vertexAttribPointer(vertTexCoord, 2, gl.FLOAT, false, 4 * 5, 4 * 3);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    gl.useProgram(null);
+}
+
+/**
  * Creates the shape object that is to be drawn; creates the vertex buffer from
  * [vertices] array. Creates the line index buffer from [mesh].line_indices 
  * and the face index buffer from [mesh].face_indices. Line indices specify 
@@ -107,6 +166,8 @@ function createShape(gl, vertices, normals, mesh) {
  */
 function drawShape(gl, program, shape, xf, proj, norm, cam_r, face_mode) {
     gl.useProgram(program);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, shape.vertexBuffer);
     var positionLocation = gl.getAttribLocation(program, "vert_position");
@@ -143,6 +204,90 @@ function drawShape(gl, program, shape, xf, proj, norm, cam_r, face_mode) {
     gl.uniformMatrix4fv(gl.getUniformLocation(program, "cameraRotation"), false, cam_r);
 
     gl.useProgram(null);
+}
+
+/**
+ * Function to create a blank float texture (taken from Lecture 7 Exhibit 0)
+ */
+function createFloatTexture(gl, width, height) {
+    gl.getExtension("OES_texture_float");
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.FLOAT, null);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    return texture;
+}
+
+/**
+ * Creates a double buffer object with a read buffer, write buffer, as well as
+ * an original_image buffer (used for the final step in bloom filter)
+ */
+function createDoubleBuffer(gl, width, height) {
+    var output = {
+        textures: [],
+        readBufferIndex: 0,
+        original_image: createFloatTexture(gl, width, height),
+        getReadBuffer: function() {
+            return this.textures[this.readBufferIndex];
+        },
+        getWriteBuffer: function() {
+            return this.textures[1 - this.readBufferIndex];
+        },
+        swap: function() {
+            this.readBufferIndex = 1 - this.readBufferIndex;
+        },
+        getOriginal: function() {
+            return this.original_image;
+        }
+    };
+    output.textures.push(createFloatTexture(gl, width, height));
+    output.textures.push(createFloatTexture(gl, width, height));
+    return output;
+}
+
+/**
+ * The first step of the bloom filter; uses a Frame Buffer Object to render the 
+ * 3D mesh scene into a 2D texture. Makes a copy of this texture and stores it
+ * in the double_buffer's original image as well.
+ */
+function fboBright(gl, fbo, double_buffer, program, shape, xf, proj, norm, cam_r, face_mode, pass) {
+    // 1. bind fbo and clear buffers
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+    // 2. attach write texture to fbo
+    if (pass == 0) {
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, double_buffer.getWriteBuffer(), 0);
+    } else {
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, double_buffer.getOriginal(), 0);
+    }
+    // 3. render as normal
+    drawShape(gl, program, shape, xf, proj, norm, cam_r, false);
+    gl.flush();
+    // 4. detach write texture
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, null, 0);
+    // 5. unbind fbo
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+}
+
+/**
+ * The blurring step of the Bloom filter; uses a Frame Buffer Object to blur the
+ * image and write it to a write buffer in [double_buffer]. 
+ */
+function fboBlur(gl, fbo, double_buffer, blur_program) {
+    // 1. bind fbo and clear buffers
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+    // 2. attach texture to fbo
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, double_buffer.getWriteBuffer(), 0);
+    // 3. render as normal
+    createAndDrawFullScreenQuad(gl, blur_program, double_buffer.getReadBuffer(), null, false);
+    gl.flush();
+    // 4. detach texture
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, null, 0);
+    // 5. unbind the fbo
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 }
 
 /**
